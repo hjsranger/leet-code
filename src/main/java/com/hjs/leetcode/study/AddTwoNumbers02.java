4b4a29d3e811b2fd12026ced312280bdd7e7d227
hjsranger <hjsranger@163.com>
2019-11-18 19:25:39 +0800

    愚蠢的结题方法

---
+++ src/main/java/com/hjs/leetcode/study/AddTwoNumbers02.java
@@ -0,0 +1,29 @@
+package com.hjs.leetcode.study;
+
+/**
+ * 两个链表相加
+ */
+public class AddTwoNumbers02 {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+
+        while (l1 != null || l1 == null){
+
+
+
+        }
+
+    }
+
+
+    public static class ListNode{
+        int val;
+        AddTwoNumbers.ListNode next;
+        ListNode(int x){
+            val = x;
+        }
+    }
+
+
+}
